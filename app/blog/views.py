be9aef98e3a4dbf211a755be69ab2fd99eeee105
LeeHanYeong <dev@lhy.kr>
2018-09-28 11:08:55 +0900

    post_list view에서 templates/blog/post_list.html을 사용

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,19 +1,29 @@
+import os
+
 from django.http import HttpResponse
-from django.utils import timezone
 
 
 def post_list(request):
-    # 현재 지역에 맞는 날짜&시간 객체 할당
-    current_time = timezone.now()
+    """
+    / (Root URL)에 해당하는 view
+    :param request:
+    :return:
+    """
+    views_file_path = os.path.abspath(__file__)
+    blog_application_path = os.path.dirname(views_file_path)
+    app_dir = os.path.dirname(blog_application_path)
+    template_path = os.path.join(app_dir, 'templates', 'blog', 'post_list.html')
 
-    return HttpResponse(
-        '<html>'
-        '<body>'
-        '<h1>Post List</h1>'
-        '<p>{}</p>'
-        '</body>'
-        '</html>'.format(
-            # 날짜&시간 객체가 가진 정보를 문자열로 변환
-            current_time.strftime('%Y. %m. %d<br>%H:%M:%S')
-        )
-    )
+    # with문 사용
+    with open(template_path, 'rt') as f:
+        content = f.read()
+
+    # 파일객체 직접 사용 후 close
+    f = open(template_path, 'rt')
+    content = f.read()
+    f.close()
+
+    # 파일객체를 변수에 할당하지 않고 사용
+    content = open(template_path, 'rt').read()
+
+    return HttpResponse(content)
